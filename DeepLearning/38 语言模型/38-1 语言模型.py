cb68bc0885790cea79a46d208b0fffbfeed8e8be
LiWin <leewenpeng@126.com>
2023-07-10 18:49:56 +0800

    新增语言模型，基于马而科夫假设

---
+++ DeepLearning/38 语言模型/38-1 语言模型.py
@@ -0,0 +1,85 @@
+import random
+import torch
+from d2l import torch as d2l
+from matplotlib import pyplot as plt
+
+
+tokens = d2l.tokenize(d2l.read_time_machine())
+corpus = [token for line in tokens for token in line]
+vocab = d2l.Vocab(corpus)
+# print(corpus[:10])
+# print(vocab.token_freqs[:10])
+
+freqs = [freq for token, freq in vocab.token_freqs]
+# d2l.plot(freqs, xlabel='token: x', ylabel='frequency: n(x)', xscale='log', yscale='log')
+# plt.show()
+
+# 其它的词元组合，比如二元语法、三元语法
+# 二元
+bigram_tokens = [pair for pair in zip(corpus[:-1],corpus[1:])]
+bigram_vocab = d2l.Vocab(bigram_tokens)
+# print(bigram_vocab.token_freqs[:10])
+
+# 三元
+trigram_tokens = [triple for triple in zip(corpus[:-2], corpus[1:-1], corpus[2:])]
+trigram_vocab = d2l.Vocab(trigram_tokens)
+# print(trigram_vocab.token_freqs[:10])
+
+bigram_freqs = [freq for token, freq in bigram_vocab.token_freqs]
+trigram_freqs = [freq for token, freq in trigram_vocab.token_freqs]
+# d2l.plot([freqs, bigram_freqs, trigram_freqs], xlabel='token: x', ylabel='frequency: n(x)', xscale='log', yscale='log',
+#          legend=['unigram', 'bigram', 'trigram'])
+# plt.show()
+
+
+# 随机生成一个小批量数据的特征和标签以供读取。在随机采样中，每个样本都是在原始的长序列上任意捕获的子序列
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    """使用随机抽样生成一个小批量子序列"""
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        return corpus[pos:pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        initial_indices_per_batch = initial_indices[i:i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):
+    """使用顺序分区生成一个小批量子序列"""
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    Xs = torch.tensor(corpus[offset:offset + num_tokens])
+    Ys = torch.tensor(corpus[offset + 1:offset + 1 + num_tokens])
+    Xs, Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_steps * num_batches, num_steps):
+        X = Xs[:, i:i + num_steps]
+        Y = Ys[:, i:i + num_steps]
+        yield X, Y
+
+my_seq = list(range(35))
+# for X, Y in seq_data_iter_random(my_seq, batch_size=2, num_steps=5):
+for X, Y in seq_data_iter_sequential(my_seq, batch_size=2, num_steps=5):
+    print("X:", X, '\nY:', Y)
+
+
+class SeqDataLoader:
+    """加载序列数据的迭代器"""
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        self.corpus, self.vocab = d2l.load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_setps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_setps)
+
