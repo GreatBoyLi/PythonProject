ce07b1da460875db054e623bd71a8112ecd08e3b
LiWin <leewenpeng@126.com>
2023-07-16 21:50:31 +0800

    新增长短期记网络LSTM

---
+++ DeepLearning/41 长短期记忆网络LSTM/40-2 长短记忆网络简单实现.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+from liwp import litorch as li
+
+batch_size, num_steps = 32, 35
+train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
+
+vocab_size, num_hiddens, device = len(vocab), 256, d2l.try_gpu()
+num_epochs, lr = 500, 1
+num_inputs = vocab_size
+lstm_layer = nn.LSTM(num_inputs, num_hiddens)
+model = d2l.RNNModel(lstm_layer, vocab_size)
+model = model.to(device)
+li.train_ch8(model, train_iter, vocab, lr, num_epochs, device)
+
+print(li.predict_ch8('time traveller', 50, model, vocab, device))
+print(li.predict_ch8('traveller', 50, model, vocab, device))
+print(li.predict_ch8('time', 50, model, vocab, device))
